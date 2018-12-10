04c663f61ed78c5a67e6519546e224a7cedd3d5f
John Eastwood <jjge732@gmail.com>
2018-12-10 12:33:29 -0600

    creation of files and some progress

---
+++ letter.js
@@ -0,0 +1,22 @@
+class Letter {
+    constructor(character, guessed = false) {
+        this.character = character;
+        this.guessed = guessed;
+    }
+
+    displayChar() {
+        if(guessed) {
+            return this.character;
+        } else {
+            return '_';
+        }
+    }
+
+    charChecker(char) {
+        if (char === this.character) {
+            guessed = true;
+        }
+    }
+};
+
+module.exports(Letter);
---
+++ word.js
@@ -0,0 +1,12 @@
+const Letter = require('./letter.js');
+
+class Word {
+    constructor(letter) {
+        this.letter = [new Letter(char)]
+    }
+    wordString() {
+        
+    }
+}
+
+module.exports();
